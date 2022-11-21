d5800195cb1a0553268b2068ebff601382d52358
Deon van Vuuren <vanvuuren.de@gmail.com>
2022-11-21 12:59:44 +0000

    Update Validation

--- Dantooine.Server/Program.cs
+++ Dantooine.Server/Program.cs
@@ -187,7 +187,7 @@ public static class Program
             {
                 //// Note: the validation handler uses OpenID Connect discovery
                 //// to retrieve the issuer signing keys used to validate tokens.
-                options.SetIssuer("https://localhost:44319/");
+                //options.SetIssuer("https://localhost:44319/");
                 ////options.AddAudiences("resource_server_2");
 
                 //// Register the encryption credentials. This sample uses a symmetric
@@ -196,13 +196,13 @@ public static class Program
                 ////
                 //// Note: in a real world application, this encryption key should be
                 //// stored in a safe place (e.g in Azure KeyVault, stored as a secret).
-                options.AddEncryptionKey(new SymmetricSecurityKey(
-                    Convert.FromBase64String("DRjd/GnduI3Efzen9V9BvbNUfc/VKgXltV7Kbk9sMkY=")));
+                //options.AddEncryptionKey(new SymmetricSecurityKey(
+                    //Convert.FromBase64String("DRjd/GnduI3Efzen9V9BvbNUfc/VKgXltV7Kbk9sMkY=")));
 
                 //// Register the System.Net.Http integration.
-                options.UseSystemNetHttp();
+                //options.UseSystemNetHttp();
                 // Import the configuration from the local OpenIddict server instance.
-                //options.UseLocalServer();
+                options.UseLocalServer();
 
                 // Register the ASP.NET Core host.
                 options.UseAspNetCore();
